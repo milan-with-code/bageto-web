8c4e554e968fd133251f64e837de1d5d8e30e8c0
milan-with-code <milan.code.github@gmail.com>
2025-08-23 20:32:59 +0530

    middlware file edit

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const protectedRoutes = ["/", "/about", "/products", "/contact", "/favorites"];
+const publicRoutes = ["/login", "/register", "/forgot-password"];
+
+export function middleware(request: NextRequest) {
+
+    const { pathname } = request.nextUrl;
+
+    console.log("🔍 Middleware running for:", request.nextUrl.pathname);
+    console.log("Cookies:", request.cookies.getAll());
+    const token = request.cookies.get("token")?.value;
+    if (publicRoutes.includes(pathname) && token) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    const isProtected =
+        pathname === "/" ||
+        protectedRoutes.some((route) => route !== "/" && pathname.startsWith(route));
+
+
+    if (isProtected && !token) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/((?!_next|api|favicon.ico).*)"],
+};
